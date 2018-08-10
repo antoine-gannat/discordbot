31a2362460a9378248f103b88bab33063b6b100c
antoine gannat <antoine.gannat@epitech.eu>
2018-08-10 10:13:51 +0200

    clean resources + modif help

--- commands/Help.js
+++ commands/Help.js
@@ -15,6 +15,8 @@ class Help extends ICommand
 		message.channel.send("Nibba u lost ?\n\
 \	\	&Say {something} #say stuff\n\
 \	\	&Shout {sound name} #join your channel and play a sound\n\
+\	\	&Shout -list #List all the shouts\n\
+\	\	&Shout -r #Play a random shout\n\
 \	\	&Sp {subreddit numberOfResults} #take a few pictures from a subreddit");
     }
 };
